c59f797f666ca7e82023d36f2b97d8aafaa4552e
chinmayharmalkar <cvharmalkar@wpi.edu>
2015-12-15 05:22:54 -0500

    change in eval
    used confusion matrix

--- Evaluate.m
+++ Evaluate.m
@@ -23,25 +23,28 @@ precision = zeros(C,1);
 recall = zeros(C,1);
 f_measure = zeros(C,1);
 gmean = zeros(C,1);
+%% New
+c_mat = confusionmat(ACTUAL,PREDICTED)
+%%
 
 for c=1:C
-    p = sum(PREDICTED==c); %positives
-    n = N-p;            %negatives (relative)
-    tp =  sum(ACTUAL==PREDICTED); %True positives
-    tn = sum(ACTUAL~=PREDICTED);  %True negatives
-    fp = n-tn;  %false positives
-    %fn = p-tp;  %false negatives
+%    p = sum(PREDICTED==c); %positives
+%    n = N-p;            %negatives (relative)
+    tp =  c_mat(c,c); %True positives
+    fp = sum(c_mat(:,1))-tp;  %false positives
+    fn = sum(c_mat(1,:))-tp;  %false negatives
+    tn = N-tp-fp-fn;  %True negatives
 
-    tp_rate = tp/p; 
-    tn_rate = tn/n;
+    tp_rate = tp/(tp+fn); 
+    tn_rate = tn/(fp+tn);
     
     accuracy(c) = (tp+tn)/N;
     sensitivity(c) = tp_rate;
     specificity(c) = tn_rate;
     precision(c) = tp/(tp+fp);
     recall(c) = tp_rate;
-    f_measure(c) = 2*((precision*recall)/(precision + recall));
-    gmean(c) = sqrt(tp_rate*tn_rate);
+    f_measure(c) = 2*((precision(c)*recall(c))/(precision(c) + recall(c)));
+    gmean(c) = sqrt(precision(c)*recall(c));
     
 end
 EVAL = [accuracy sensitivity specificity precision recall f_measure gmean];
